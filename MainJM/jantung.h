ac055681ed3ba549c17af37d528cc3616e0da857
rafiazharr17 <rafipilo17@gmail.com>
2023-05-24 16:26:13 +0700

    membuat header jantung

--- MainJM/jantung.h
+++ MainJM/jantung.h
@@ -1,1 +1,15 @@
-#pragma once
+#ifndef JANTUNG_H
+#define JANTUNG_H
+
+using namespace std;
+
+class jantung {
+public:
+	jantung() {
+		cout << "jantung dihidupkan\n";
+	}
+	~jantung() {
+		cout << "jantung dimatikan\n";
+	}
+};
+#endif 
